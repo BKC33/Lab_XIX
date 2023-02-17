e84aa1ddd39519070316f1b459982869ecd9d097
BestChennn <bestchenbestchen@gmail.com>
2023-02-17 22:16:40 +0700

    Complete

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -3,10 +3,10 @@
 #include<vector>
 #include<string>
 #include<cstdlib>
-
+#include<cstring>
 using namespace std;
 
-char score2grade(int score){
+char score2grade(int score){  
     if(score >= 80) return 'A';
     if(score >= 70) return 'B';
     if(score >= 60) return 'C';
@@ -20,23 +20,70 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
+void importDataFromFile(string filename,vector<string> &names,vector<int> &scroes,vector<char> &grades){  
+    ifstream source;
+    source.open(filename.c_str());
+    string Newfile;
+    char format[] = "%[^:]: %d %d %d";
+    char name[100];
+    int s1,s2,s3;
+   
+    while(getline(source,Newfile)){
+        sscanf(Newfile.c_str(),format,name,&s1,&s2,&s3);
+        names.push_back(name);
+        scroes.push_back(s1+s2+s3);
+        grades.push_back(score2grade(s1+s2+s3));
+    }
+
+ source.close();
+         
+    }
+    
+void getCommand(string &command,string &key){
+    string kw;
+    int gap;
+    cout<<"Please input your command: ";
+    getline(cin,kw);
+    gap = kw.find_first_of(" ");   
+    command = kw.substr(0,gap);   
+    key = kw.substr(gap+1);  
 
 }
 
-void getCommand(){
+void searchName(vector<string> name,vector<int> scores,vector<char> grades,string key){
+    bool check = false;
+    for(unsigned int i=0;i<name.size();i++){
+        if(key == toUpperStr(name[i])){
 
+            check = true;
+
+            cout<<"---------------------------------"<<'\n';
+            cout<< name[i] <<"'s score = "<< scores[i] <<'\n';
+            cout<< name[i] <<"'s grade = "<< grades[i] <<'\n';
+            cout<<"---------------------------------"<<'\n';
+        }
+    }
+
+if(check == false){
+    cout<<"---------------------------------"<<'\n';
+    cout<< "Cannot found."<<'\n';
+    cout<<"---------------------------------"<<'\n';
+}
+    
 }
 
-void searchName(){
+void searchGrade(vector<string> name,vector<int> scores,vector<char> grades,string key){
+    cout<<"---------------------------------"<<'\n';
+   
+    for(unsigned int i =0;i<grades.size();i++){
 
+        if(key[0] == grades[i]){
+            cout<< name[i] <<" ("<<scores[i]<<")"<<'\n';
+        }
+    }
+        cout<<"---------------------------------"<<'\n';
 }
-
-void searchGrade(){
-
-}
-
-
+        
 int main(){
     string filename = "name_score.txt";
     vector<string> names;
